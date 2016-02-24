9ed5b4ef8cbc27b3a6113ada30a22f28febafe9b
Pistillo Stefano <stefano.pistillo@ericsson.com>
2016-02-24 15:30:31 +0100

    update

--- webapp/public/javascripts/controllers.js
+++ webapp/public/javascripts/controllers.js
@@ -1,10 +1,19 @@
 var sensorLogControllers = angular.module('sensorLogControllers', []);
 
-sensorLogControllers.controller('SensorLogList', ['$scope', 'SensorLog', function($scope, SensorLog) {
-		console.log("SENSORS before ->"+result);
-		var result = SensorLog.query();
-		console.log("SENSORS ->"+result);
-		$scope.prova = result;
+sensorLogControllers.controller('SensorLogList', ['$scope', '$http', function($scope, $http) {
+		
+		//var result = SensorLog.query();
+		//console.log("SENSORS ->"+result);
+		
+		$http.get('sensorLog').success(function(data) {
+				console.log("Data->"+data);
+				var p = JSON.parse(data);
+				console.log("Data->"+p);
+			  $scope.sensorLog = JSON.parse(data);
+		});
+
+		
+		//$scope.prova = result;
 }]);
 
 
--- webapp/routes/sensorLog.js
+++ webapp/routes/sensorLog.js
@@ -11,8 +11,8 @@ var SensorLog = function(id_in, status_in){
 
 /* GET SensorLog listing. */
 router.get('/', function(req, res, next) {
-  var sensorLogArray = [new SensorLog(0,'aperto'), new SensorLog(0,'chiuso')];
-  res.send(JSON.stringify(sensorLogArray));
+  var sensorLogArray = [new SensorLog(0,'aperto'), new SensorLog(1,'chiuso')];
+  res.json(JSON.stringify(sensorLogArray));
 });
 
 module.exports = router;
