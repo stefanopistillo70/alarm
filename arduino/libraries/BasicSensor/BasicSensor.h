ab3597ea7a28eaa81a0c4c74c84bb650f9f74ade
Pistillo Stefano <stefano.pistillo@ericsson.com>
2016-03-03 18:29:54 +0100

    update

--- arduino/libraries/BasicSensor/BasicSensor.h
+++ arduino/libraries/BasicSensor/BasicSensor.h
@@ -12,7 +12,7 @@ class BasicSensor {
 	private:
 
 	const int BATTERY_SENSE_PIN = A0;
-	unsigned long SLEEP_TIME = 5000;  // sleep time between reads (seconds * 1000 milliseconds)
+	unsigned long SLEEP_TIME = 3000;  // sleep time between reads (seconds * 1000 milliseconds)
 	int oldBatteryPcnt = 0;
 	MySensor *gw;
 
--- arduino/sensors/CounterSensor/CounterSensor.ino
+++ arduino/sensors/CounterSensor/CounterSensor.ino
@@ -6,28 +6,22 @@
 #include "BasicSensor.h"
 
 
+
 class TestSensor : public BasicSensor {
 	unsigned long counter = 0;
-	void processSensor(MySensor *gw);
+	public:
+	void processSensor(MySensor * gw);
 };
 
-static inline MyMessage& build (MyMessage &msg, uint8_t sender, uint8_t destination, uint8_t sensor, uint8_t command, uint8_t type, bool enableAck) {
-	msg.sender = sender;
-	msg.destination = destination;
-	msg.sensor = sensor;
-	msg.type = type;
-	mSetCommand(msg,command);
-	mSetRequestAck(msg,enableAck);
-	mSetAck(msg,false);
-	return msg;
-}
-void TestSensor::processSensor(MySensor *gw) {
+void TestSensor::processSensor(MySensor * gw) {
 	Serial.println("Sending counter ...");
-	MyMessage msg;
-	gw->sendRoute(build(msg, gw->getNodeId(), GATEWAY_ADDRESS, NODE_SENSOR_ID, C_INTERNAL, I_LOG_MESSAGE, true).set(counter));
+	int id = gw->getNodeId();
+	MyMessage msg(id, V_TRIPPED);
+	gw->send(msg.set(counter));
 	counter++;
 	Serial.println("Msg Sent");
 }
+
 
 TestSensor sensor;
 
