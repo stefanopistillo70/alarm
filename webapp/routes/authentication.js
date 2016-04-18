512e843f065485fb66061b3b7d2123c83f98b7fd
Pistillo Stefano <stefano.pistillo@ericsson.com>
2016-04-18 11:14:33 +0200

    update

--- webapp/routes/authentication.js
+++ webapp/routes/authentication.js
@@ -19,73 +19,71 @@ var configAuth = {
 
 var passport = require('passport');
 
-    // used to serialize the user for the session
-    passport.serializeUser(function(user, done) {
-        done(null, user.id);
-    });
+// used to serialize the user for the session
+passport.serializeUser(function(user, done) {
+	console.log("SERIALIZE USER");
+	done(null, user.id);
+});
 
-    // used to deserialize the user
-    passport.deserializeUser(function(id, done) {
-        User.findById(id, function(err, user) {
-            done(err, user);
-        });
-    });
+// used to deserialize the user
+passport.deserializeUser(function(id, done) {
+	console.log("DESERIALIZE USER");
+	User.findById(id, function(err, user) {
+		done(err, user);
+	});
+});
     
-    // code for login (use('local-login', new LocalStategy))
-    // code for signup (use('local-signup', new LocalStategy))
-    // code for facebook (use('facebook', new FacebookStrategy))
-    // code for twitter (use('twitter', new TwitterStrategy))
 
-    // =========================================================================
-    // GOOGLE ==================================================================
-    // =========================================================================
-    passport.use(new GoogleStrategy({
+// =========================================================================
+// GOOGLE ==================================================================
+// =========================================================================
+passport.use(new GoogleStrategy({
 
-        clientID        : configAuth.googleAuth.clientID,
-        clientSecret    : configAuth.googleAuth.clientSecret,
-        callbackURL     : configAuth.googleAuth.callbackURL,
+		clientID        : configAuth.googleAuth.clientID,
+		clientSecret    : configAuth.googleAuth.clientSecret,
+		callbackURL     : configAuth.googleAuth.callbackURL,
 
-    },
-    function(token, refreshToken, profile, done) {
+	},
+	function(token, refreshToken, profile, done) {
 
-        // make the code asynchronous
-        // User.findOne won't fire until we have all our data back from Google
-        process.nextTick(function() {
-			console.log("NEXT TRIK");
-			console.log(token);
-            // try to find the user based on their google id
-            User.findOne({ 'google.id' : profile.id }, function(err, user) {
-                if (err)
-                    return done(err);
+		// make the code asynchronous
+		// User.findOne won't fire until we have all our data back from Google
+		process.nextTick(function() {
+			console.log("NEXT TRIK token->"+token);
+			// try to find the user based on their google id
+			User.findOne({ 'google.id' : profile.id }, function(err, user) {
+				if (err)
+					return done(err);
 
-                if (user) {
+				if (user) {
 
-					console.log("User Found");
-                    // if a user is found, log them in
-                    return done(null, user);
-                } else {
-                    // if the user isnt in our database, create a new user
-                    var newUser          = new User();
+					console.log("User Found token ->"+user.google.token);
+					// if a user is found, log them in
+					return done(null, user);
+				} else {
+					// if the user isnt in our database, create a new user
+					var newUser          = new User();
 
-                    // set all of the relevant information
-                    newUser.google.id    = profile.id;
-                    newUser.google.token = token;
-                    newUser.google.name  = profile.displayName;
-                    newUser.google.email = profile.emails[0].value; // pull the first email
+					// set all of the relevant information
+					newUser.google.id    = profile.id;
+					newUser.google.token = token;
+					newUser.google.name  = profile.displayName;
+					newUser.google.email = profile.emails[0].value; // pull the first email
 
 					console.log("NEW User");
 					console.log(newUser);
-                    // save the user
-                    newUser.save(function(err) {
-                        if (err)
-                            throw err;
-                        return done(null, newUser);
-                    });
-                }
-            });
-        });
+					// save the user
+					newUser.save(function(err) {
+						if (err)
+							throw err;
+						return done(null, newUser);
+					});
+				}
+			});
+		});
 
-    }));
+	})
+);
 
 
 
@@ -103,13 +101,13 @@ var router = express.Router();
 router.get('/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
 
 // the callback after google has authenticated the user
-router.get('/google/callback',
-		passport.authenticate('google', {
-				successRedirect : '/#/',
-				failureRedirect : '/#/login'
-		}));
-
-
+router.get('/google/callback', 
+		passport.authenticate('google', { failureRedirect: '/#/login' }),
+		function(req, res) {
+			console.log("AUTH -> SUCCESS");
+			res.redirect('/');
+		}
+);
 
 module.exports = router;
 
