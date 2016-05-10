e66ad9eaf8e32a1abd935b7a899996fbd964d189
Pistillo Stefano <stefano.pistillo@ericsson.com>
2016-05-10 18:27:24 +0200

    update

--- environment/Production/chef/roles/vagrant-production-box.rb
+++ environment/Production/chef/roles/vagrant-production-box.rb
@@ -9,19 +9,19 @@ default_attributes(
 		"version" => "5.6.0",
 		"checksum_linux_x64" => "6b10e446b5a1227673b87d840e9a500f5d2dbd2b806d96e2d81d634c3381a5f1"
 	},
-	"mongodb" => {
+	"mongodb3" => {
 		"config" => {
-			"auth" => "true"
-		},
-		"admin" => {
-			"username" => "admin",
-			"password" => "password"
+			"mongod" => {
+					"net" => {
+						"bindIp" => "127.0.0.1"
+					}
+			}
 		}
 	}
 )
 
 
 run_list(
-	"recipe[mongodb]",
+	"recipe[mongodb3]",
 	"recipe[nodejs::install_from_binary]"
 )
