70067ddbc7d0de2028336f34f68cff3e58cdc65f
Pistillo Stefano <stefano.pistillo@ericsson.com>
2016-06-01 15:14:39 +0200

    update

--- controller/protocol_433.js
+++ controller/protocol_433.js
@@ -5,15 +5,27 @@ var Repository = require('./repository.js');
 var Protocol_433 = function(repository) {
 						
 		this.onMsg = function(rd,callback){
-			repository.buildNewDevice("433", rd, function(device,err){
-				if(device){
-					console.log(device);
+			
+			var deviceId = rd;
+			
+			var device = repository.getDevice(deviceId);
+			
+			if(device){
+				var event = {deviceId : deviceId, sensorId: 0, event : "Allarm"}
+				repository.addEventLog(event, function(){
 					callback(device);
-				}else{
-					console.log(err);
-					callback(err);
-				}
-			});
+				});
+			}else{
+				repository.buildNewDevice("433", rd, function(device,err){
+					if(device){
+						console.log(device);
+						callback(device);
+					}else{
+						console.log(err);
+						callback(err);
+					}
+				});
+			}
 		};
 };
 
