aa5ecca7cc789052a1b52a578442cf1833cde8fa
Pistillo Stefano <stefano.pistillo@ericsson.com>
2016-07-21 06:46:12 +0200

    update

--- controller/controller.js
+++ controller/controller.js
@@ -23,14 +23,14 @@ controller.start = function(){
 	
 	//Select gateway comunication type
 	var GW;
-	var gw_type = "i2c";
-	//var gw_type = "test";
+	//var gw_type = "i2c";
+	var gw_type = "serial";
 	if(gw_type === 'test') GW = require('./test/sim_gateway.js');
 	else if(gw_type === 'serial') GW = require('./serial_gateway.js');
 	else if(gw_type === 'i2c') GW = require('./i2c_gateway.js');
 	
-	var gateway_433 = new GW(protocol_433,'\\\\.\\COM10',9600);
-	//var gateway_433 = new GW(protocol_433);
+	//var gateway_433 = new GW(protocol_433,'\\\\.\\COM10',9600);
+	var gateway_433 = new GW(protocol_433);
 	//var gateway_NRF = new GW(mysp_15,'\\\\.\\COM10',115200);
 
 
@@ -39,10 +39,9 @@ controller.start = function(){
 		controller.repository.getRemoteUpdate(function(){
 			logger.log('info','Remote Update DONE.');
 			//TODO uncomment
-			//controller.repository.checkForRemoteUpdate();
+			controller.repository.checkForRemoteUpdate();
 			
-			
-			controller.repository.uploadFile();
+			//controller.repository.uploadFile();
 		});
 	});	
 	
